c54e3550aee9b04849522369a8840ebef217e8ab
Matt Callaghan <mcallaghan@naperville203.org>
2021-02-15 19:39:04 -0600

    Update chapter_6_notes_2_starter.py

--- chapter_6_notes_2_starter.py
+++ chapter_6_notes_2_starter.py
@@ -1,58 +1,30 @@
-# Chapter 6 guided notes #2
+# Chapter 6 notes #2
 
-# ENCAPSULATION
-#   Helps keep code independent by hiding (or encapsulating) the
-#   details. Variables(including parameters) created inside a
-#   function cannot be directly accessed outside the function.
-
-# 1: GLOBAL vs. LOCAL SCOPE
-#   Scope defines the accessibility of separate parts of a program. Functions
+# SCOPE defines the accessibility of separate parts of a program. Functions
 #       have a local scope by default, which means variables (including parameters)
-#       initialized within a function only exist within that particular function. 
-#       Global variables initialized outside of functions are accessible throughout 
+#       initialized within a function are LOCAL VARIBALES, and only exist within that particular function.
+
+
+# GLOBAL vs. LOCAL SCOPE
+#       GLOBAL VARIABLES initialized outside of functions are accessible throughout 
 #       a program, and therefore have a global scope.
  
-# Global Reach Example - demonstrates local and global variables
-
+# Global Reach Example - demonstrates global variable access
 #   define functions
 
-#   main program
 
-input("\nPress the enter key to continue.")
-
-# CHALLENGE: create a new function called make_omelette() that prints the ingredients
-#   for an omelette. Pass in the ingredient(s) as a paramenter (local variable) and use a 
-#   global ingredient (eggs) to make at least 2 different omelettes.
+# Shadowing - When a local variable has the same name as a global variable we say that the local shadows the global. 
+#  A shadow means that the global variable cannot be accessed by Python because the local variable will be found first. 
+#  This is another good reason not to use global variables. 
 
 
+# main program 
+
+
+# ENCAPSULATION - Keeps code logically independent by ‘hiding’ information from other parts of a program.
+#  Functions should be written to leverage encapsulation and therefore only rely on local variables (if possible).
 
 input("Press enter to continue.")
 
-# 2: main function
+# MAIN FUNCTION
 #   encapsulates main program
-
-#   define functions
-def grade_ave(score1, score2, score3):
-    """Calculates the average of 3 grades"""
-    total = score1 + score2 + score3
-    grade_ave = total/3
-    return grade_ave
-
-#   define main function
-#print("Welcome!")
-#score = grade_ave(80,90,85)
-#print(score)
-
-#   call main function
-
-input("Press enter to continue.")
-
-# 3: unlimited number of arguments
-#   Certain functions(like print()) need to handle any number of arguments.
-#   Use asterisk before parameter name; turns parameter into tuple of X length.
-
-# CHALLENGE: Update the grade_ave() function to only have one
-#   parameter so it can take in any number of student grades and return the 
-#   average grade.
-
-
