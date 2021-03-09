8581359db396b6cb8ee645cafaa649f662b21d0b
Matt Callaghan <mcallaghan@naperville203.org>
2021-03-09 08:50:23 -0600

    Update chapter_6_notes_3_finished.py

--- chapter_6_notes_3_finished.py
+++ chapter_6_notes_3_finished.py
@@ -1,83 +1,63 @@
 # day 3 notes on functions
+import random
 
 # unlimited number of arguments
-#   Certain functions(like print()) need to handle any number of arguments.
-#   Use asterisk before parameter name; turns parameter into tuple of X length.
+#	  Certain functions(like print() ) need to handle any number of arguments
+#		Use asterisk before parameter name; turns parameter into a tuple of X length.
 
-def grade_ave(*args):
-    """Calculates the average from any number of grades"""
-    print ("Tuple created from arguments:", args) 
-    total=0
-    for i in args:
-        total+=i
-    grade_ave= total/len(args)
-    return grade_ave
+def grade_avg(*args):
+	"""Calculates the average from any number of grades"""
+	print("Tuple created from arguments:", args)
+	total = 0
+	for i in args:
+		total += i
+	grade_ave = total / len(args)
+	return grade_ave
 
-print(grade_ave(80, 82, 88, 90))
-print(grade_ave(60, 70, 80, 90, 100))
-
-# CHALLENGE: create a new function called make_omelette() that prints the ingredients
-#   for an omelette. Pass in unlimited ingredient(s) as a paramenter (local variable) and use a 
-#   global ingredient (eggs) to make at least 2 different omelettes with different ingredients.
-
-eggs = "Eggs" # global ingredient
-
-def make_omelette(*ingredients): # parameter defines local ingredients
-    """Makes omelettes using both local and global variable ingredients and"""
-    print("\nYou made an omelette with:\n" + eggs)
-    for i in ingredients:
-        print(i)
-    
-make_omelette("Peppers", "Onion", "Tomatoes")
-make_omelette("Cheese", "Broccoli", "Fritos")
-
+ave = grade_avg(80, 82, 84, 86, 88)
+print("The grade average is:", ave)
+print("The grade average is:", grade_avg(100,90, 80))
 
 # default parameter values
 #   you have the option to assign a default value, so that if no
 #   value is passed to the function it will use the value assigned
-#   trap: once you assign a default value to one parameter,
-#   you must assign one to all parameters.
-def bday(name="Missing", age=0):
+#   trap: once you assign a default value to one parameter, you must assign one to all parameters.
+def bday(name = "Missing", age = 0):
     """prints name and birthday"""
     print ("Wow", name, "you are", age, "years old!")
-bday()
+bday(age = 11, name = "Frank") # keyword arguments
+bday("Heather", 36) # positional arguments
+bday() # relying on default parameters
+bday("Tim") # combining positional and default parameters
 
-# can override default with call (positional or keyword arguments)
- # POSITIONAL ARGUMENTS - arguments passed in the same order as the parameters are specified
- # KEYWORD ARGUMENTS - arguments passed in a different order than the parameters are specified
-bday("Matt",33)
-bday("Matt")  # default parameters allow you to omit arguments from the function call
-# bday()  # still required to send other arguments not asssigned a default value
-bday(age = 33, name = "Frank") # using keyword arguments 
+
+# can override default parameter at function call (using either positional or keyword arguments)
+#  POSITIONAL ARGUMENTS - arguments passed in the same order as the parameters are specified
+#  KEYWORD ARGUMENTS - arguments passed in a different order than the parameters are specified
 
 # How might this be useful?
-#   Imagine you are writing a battle() function and want to assign the
-#     enemy a default health of 100. You can use a default parameter for that.
+#   Imagine you are writing a battle() function for the Oregon Trail and want to assign the
+#     potential enemy a default health of 100. You can use a default parameter for that.
 
-import time, random
+def battle(enemy_hp = 100):
+	"""a battle between a player and an enemy"""
+	pass
 
-def battle(enemy_hp = 100): # enemy has full health by default (100)
-    """a battle between a player and an enemy"""
-    global hp
-    flee = False
-    while True:
-        time.sleep(1)
-        print("Player health:", hp, "\nEnemy health:", enemy_hp)
-        hp -= 10
-        enemy_hp -= 50
-        flee = True
-        if flee:
-            break
-    return enemy_hp
+battle()
+battle(50)
+battle(1000)
 
-# global scope
-hp = 100  # player starting health level
+# CHALLENGE: create a new function called make_omelette() that prints the ingredients for an omelette. 
+#   Pass in unlimited ingredient(s) as a paramenter (local variable) and use a global ingredient (eggs) 
+#   to make at least 2 different omelettes with different ingredients.
 
-e_hp = battle() # enemy may still be alive!
+eggs = "Eggs" # global ingredient
 
-print("Player health outside function:", hp, "\nEnemy health outside function:", e_hp)
+def make_omelette(*ingredients):  # paramenter defines an unlimited number of local ingredients
+	print("\nYou made an omelette with:\n" + eggs)
+	for ing in ingredients:
+		print(ing)
 
-if 
-e_hp = battle(e_hp)
-
-print("Player health outside function:", hp, "\nEnemy health outside function:", e_hp)
+make_omelette("Peppers", "Onion", "Tomatoes")
+make_omelette("Cheese", "Bacon")
+make_omelette("Fritos", "Olives", "Fish", "Toothpaste")
