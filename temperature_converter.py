5047ec7e959e158d06d8134b0b98d17185f23ff8
Matt Callaghan <mcallaghan@naperville203.org>
2025-01-27 10:30:12 -0600

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,17 +1,21 @@
 # Functions t0 convert between Celsius and Fahrenheit.
 
-# DOCSTRING - 
+# DOCSTRING: A special way to document a function;
+#    It must be triple quoted (single or double), and on the first line after the function header. 
+#    Can provide useful information when attempting to use the function.
 
 def celsius_to_fahrenheit(celsius):
     """converts a temperature from celsius to fahrenheit"""
-    return celsius * 1.8 + 32
+    fahr = celsius * 1.8 + 32
+    return fahr
 
 # This function takes a temperature
 # in Fahrenheit and converts it to
 # Celsius.
 def fahrenheit_to_celsius(fahrenheit):
     """converts a temperature from fahrenheit to celsius"""
-    return (fahrenheit - 32) / 1.8
+    celc = (fahrenheit - 32) / 1.8
+    return celc
 
 print("0C In F: " + str(celsius_to_fahrenheit(0)))
 print("100C In F: " + str(celsius_to_fahrenheit(100)))
