dff68461690bccfb140140830ea8900aac007a0d
Matt Callaghan <mcallaghan@naperville203.org>
2025-01-27 10:28:20 -0600

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,13 +1,16 @@
-# This function takes a temperature
-# in Celsius and converts it to
-# Fahrenheit.
+# Functions t0 convert between Celsius and Fahrenheit.
+
+# DOCSTRING - 
+
 def celsius_to_fahrenheit(celsius):
+    """converts a temperature from celsius to fahrenheit"""
     return celsius * 1.8 + 32
 
 # This function takes a temperature
 # in Fahrenheit and converts it to
 # Celsius.
 def fahrenheit_to_celsius(fahrenheit):
+    """converts a temperature from fahrenheit to celsius"""
     return (fahrenheit - 32) / 1.8
 
 print("0C In F: " + str(celsius_to_fahrenheit(0)))
